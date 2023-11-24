62a2b6669fe9e7227803b1afe734873c304b595e
Robpiot <robin.piot@outlook.com>
2023-11-24 14:27:24 +0100

    ls fixed + few features to fix like line-through, add delete and others stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
+const LSKEY = "inputs";
 const Title = () => (
-  <h1 className=" bg-gray-200 text-4xl text-center p-20 w-full">To Do List</h1>
+  <h1 className="w-full p-20 text-4xl text-center bg-teal-200 ">To Do List</h1>
 );
 
 const Subtitle = () => (
-  <h2 className="text-xl text-center p-10 bg-gray-200">Things to do</h2>
-);
-
-const InputResult = (props) => (
-  <div className="flex p-10">
-    <input type="checkbox" />
-    <p className="ml-2">{props.text}</p>
-  </div>
+  <h2 className="p-10 text-xl text-center bg-teal-200">Things to do</h2>
 );
 
 const InputForm = () => {
-  const [inputs, setInputs] = React.useState([]);
   const [inputValue, setInputValue] = React.useState("");
+  const [checkedItems, setCheckedItems] = React.useState({});
+
+  const savedInputs = JSON.parse(localStorage.getItem("inputs")) || [];
+  const [inputs, setInputs] = React.useState(savedInputs);
+
+  useEffect(() => {
+    localStorage.setItem("inputs", JSON.stringify(inputs));
+  }, [inputs]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -25,33 +26,59 @@ const InputForm = () => {
     setInputValue("");
   };
 
+  function handleChange(event, index) {
+    setCheckedItems((prev) => ({ ...prev, [index]: event.target.checked }));
+  }
+
+  const InputResult = (props) => (
+    <div className="flex p-10">
+      <input
+        type="checkbox"
+        onChange={(e) => props.handleChange(e, props.index)}
+      />
+      <p
+        className="p-2 text-xl"
+        style={{
+          textDecoration: checkedItems[props.index] ? "line-through" : "none",
+        }}
+      >
+        {props.text}
+      </p>
+    </div>
+  );
+
   return (
-    <div className="w-screen h-screen justify-items-center bg-white">
+    <div className="w-screen h-screen bg-white justify-items-center">
       <Title />
 
       <form
-        className="w-full max-w-sm flex justify-center"
+        className="flex justify-center w-full max-w-sm"
         onSubmit={handleSubmit}
       >
-        <div className="flex items-center justify-center border-b border-teal-500 py-6 m-10">
+        <div className="flex items-center justify-center py-6 m-10 border-b border-teal-500">
           <input
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="w-full px-2 py-1 mr-3 leading-tight text-gray-700 bg-transparent border-none appearance-none focus:outline-none"
           />
           <button
             type="submit"
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="flex-shrink-0 px-2 py-1 text-sm text-white bg-teal-500 border-4 border-teal-500 rounded hover:bg-teal-700 hover:border-teal-700"
           >
             Submit
           </button>
-        </div>{" "}
+        </div>
       </form>
       <Subtitle />
       <div className="">
         {inputs.map((input, index) => (
-          <InputResult key={index} text={input} />
+          <InputResult
+            key={index}
+            text={input}
+            handleChange={handleChange}
+            index={index}
+          />
         ))}
       </div>
     </div>
